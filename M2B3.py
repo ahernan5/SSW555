9b926df88d63126fa0f1d9ca4128f373d5b12832
Alyssa Hernandez <alllyy152@gmail.com>
2022-09-25 16:19:19 -0400

    Update M2B3.py

--- M2B3.py
+++ M2B3.py
@@ -1,6 +1,6 @@
 #Alyssa Hernandez
 #I pledge my honor that I have abided by the Stevens Honor System. -AH 
-
+#NOTE: GitHub repository name: SSW555
 
 #opening the GEDCOM file for reading 
 GedcomFile = open('M1B6.txt', 'r')
